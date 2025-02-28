5b9fd92a477f9fbad1a0da77680c0b8796b2af98
liseth giraldo <70545096+lisethgira@users.noreply.github.com>
2025-02-27 22:47:24 -0500

    feat(sidebar): se crean iconos y navegación

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,39 +1,45 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild, AfterViewInit, HostListener} from '@angular/core';
+import { faBars, faBuilding, faNewspaper, faUser } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [FontAwesomeModule, CommonModule],
+  standalone: true,
+  imports: [FontAwesomeModule, CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent implements AfterViewInit {
+export class SidebarComponent {
   @ViewChild('sidebar') sidebar!: ElementRef;
   @ViewChild('toggleBtn') toggleBtn!: ElementRef;
 
-  ngAfterViewInit() {
-    // Solución: Usar setTimeout para esperar a que Angular inicialice las referencias
-    setTimeout(() => {
-      if (this.toggleBtn && this.sidebar) {
-        this.toggleBtn.nativeElement.addEventListener("click", () => {
-          this.sidebar.nativeElement.classList.toggle("expand");
-        });
-      }
-    });
+  open = false;
+  isMobile = false;
+  faBars = faBars;
+  faBuilding = faBuilding;
+  faNewspaper = faNewspaper;
+  faUser = faUser;
+
+  constructor() {
+    this.checkScreenSize();
   }
 
-  // isOpen = false;
-  // toggleSidebar() {
-  //   this.isOpen = !this.isOpen;
-  // }
+  checkScreenSize(): void {
+    if (typeof window !== 'undefined') {
+      this.isMobile = window.innerWidth < 768;
+    }
+  }
 
-  open = false;
-  faBars = faBars;
+  @HostListener('window:resize', [])
+  onResize(): void {
+    this.checkScreenSize();
+  }
 
+  /** Abre y cierra el sidebar */
   toggleMenu() {
     this.open = !this.open;
   }
-
 }
+
