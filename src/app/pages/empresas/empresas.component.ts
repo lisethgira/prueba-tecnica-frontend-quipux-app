0ce009aab4bd2f089ce034154059f8d413bed9b6
Liseth Giraldo <lisethgiraldo628@gmail.com>
2025-02-28 00:26:39 -0500

    feat(empresas): Se crean cards donde se visualiza la informacion de los JSON

--- src/app/pages/empresas/empresas.component.ts
+++ src/app/pages/empresas/empresas.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
+import empresaData from '../../../assets/data/info-empresa.json';
+import contactoData from '../../../assets/data/info-contacto.json';
 
 @Component({
   selector: 'app-empresas',
-  imports: [],
+  standalone: true, // Asegúrate de que este componente sea standalone
+  imports: [CommonModule], // Agrega CommonModule aquí
   templateUrl: './empresas.component.html',
-  styleUrl: './empresas.component.css'
+  styleUrls: ['./empresas.component.css']
 })
 export class EmpresasComponent {
-
+  empresa = empresaData[0].empresa;
+  contacto = contactoData[0].contacto;
 }
